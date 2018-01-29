5c48c8dbf37f4ea717128fb6de8472452b04a513
Evangeline VIllena <vvanjie@yahoo.com>
2018-01-29 02:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// write your code below!
+function happyHolidays(){// write your code below!
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays,' + ${name}!`
+}
